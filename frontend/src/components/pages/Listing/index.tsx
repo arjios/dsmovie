5e3f4a23e3e52e331d365d8ec468da26989be29c
Arjios <antonio.uab.ios.fisica@gmail.com>
2022-01-14 11:33:04 -0300

    useState, useEffect

--- frontend/src/components/pages/Listing/index.tsx
+++ frontend/src/components/pages/Listing/index.tsx
@@ -1,7 +1,21 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "Types/movie";
+import { BASE_URL } from "utils/request";
 
 function Listing() {
+  
+  const [pageNumber, setPageNumber] = useState(0);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=0`).then((response) => {
+      const data = response.data as MoviePage;
+      setPageNumber(data.number);
+    });
+  }, []);
+
   return (
     <>
       <Pagination />
@@ -23,7 +37,6 @@ function Listing() {
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
           </div>
-
         </div>
       </div>
     </>
